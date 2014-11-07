66df8d56e553406723957f3dba7e487c9c0e5b89
kl <kl@10.66.3.126>
2014-11-07 12:19:44 +0600

    добавление номера телефона

--- lib/config/plugin.php
+++ lib/config/plugin.php
@@ -4,7 +4,7 @@ return array(
     'name' => 'RedHelper',
     'description' => 'Онлайн консультант для сайта',
     'vendor' => '985310',
-    'version' => '1.0.1',
+    'version' => '1.0.2',
     'img' => 'img/redhelper.png',
     'frontend' => true,
     'shop_settings' => true,
